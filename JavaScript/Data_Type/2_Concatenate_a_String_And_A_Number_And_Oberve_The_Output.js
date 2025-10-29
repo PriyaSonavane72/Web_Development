0053f695d766b75b37b951a7fa00191d4ee4a4d0
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-10-29 19:53:19 +0530

    Add files via upload

---
+++ JavaScript/Data_Type/2_Concatenate_a_String_And_A_Number_And_Oberve_The_Output.js
@@ -0,0 +1,13 @@
+// let  name = "Priya";
+// let num = 1455;
+
+// let result = name + num ;
+// console.log(result);
+
+
+let name = "Priya";
+let age = 20;
+
+let result = name + age;
+
+console.log(result);
