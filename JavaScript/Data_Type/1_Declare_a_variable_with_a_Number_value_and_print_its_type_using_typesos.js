9c95a4cd2ff8b9c9ca58a5a54b545a7e053b1561
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-10-29 19:51:54 +0530

    Add files via upload

---
+++ JavaScript/Data_Type/1_Declare_a_variable_with_a_Number_value_and_print_its_type_using_typesos.js
@@ -0,0 +1,19 @@
+///01_Declare_a_variable_with_a_Number_value_and_print_its_type_using_typesos.html -->
+
+var num = 1455;
+ num = 2005;
+console.log(num);
+console.log(typeof(num));
+
+
+let age = 20;
+age= 21;   
+console.log(age);
+console.log(typeof(age));
+
+
+const a = 12;
+const b = 21;
+ const sum = a+b;
+ console.log(sum);
+ console.log(typeof(sum));
