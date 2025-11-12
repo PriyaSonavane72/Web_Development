5cad3e46e76e7901434142ccad2fbda2b28035e7
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-11-12 21:11:18 +0530

    Add files via upload

---
+++ JavaScript/Function/4_Demonstrate_A_Function_With_Default_Parameters.js
@@ -0,0 +1,8 @@
+function defaultMessage(name = "Guest",Message = "Welcome To Our Javascript")
+{
+    console.log(`Hellow ${name}, ${Message}`);
+};
+
+defaultMessage();
+defaultMessage("Priya");
+defaultMessage("Priya","Have A Good Day");
