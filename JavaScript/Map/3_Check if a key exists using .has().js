5b89b37901557d09084ac5b1ac56b72c37b68150
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-11-13 20:09:49 +0530

    Add files via upload

---
+++ JavaScript/Map/3_Check if a key exists using .has().js
@@ -0,0 +1,10 @@
+///Check if a key exists using .has().
+ 
+let Array =  new Map();
+Array.set("name","Vaishnaavi"),
+Array.set("age",21);
+Array.set("Course","BCS");
+
+
+console.log(Array.has("age"));
+console.log(Array.has("PRN No"));
