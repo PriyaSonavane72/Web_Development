a25423189345b0fd111bd2b9400c9b6b28750276
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-11-14 22:23:55 +0530

    Add files via upload

---
+++ JavaScript/Map/4_Get a value using .get().js
@@ -0,0 +1,11 @@
+///Get a value using .get().
+ let Array = new Map();
+
+ Array.set("name","Vaishnavi");
+ Array.set("Age",23);
+ Array.set("Course","BCA");
+
+ console.log(Array.get("name"));
+ console.log(Array.get("Age"));
+ console.log(Array.get("Course"));
+ console.log(Array.get("PRN No"));
