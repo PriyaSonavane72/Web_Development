9752da4b31636fe1e364223f05ef7c7064b5348b
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-11-18 23:22:02 +0530

    Add files via upload

---
+++ JavaScript/Object/1_Create_a_Student_Object_With_Properties.js
@@ -0,0 +1,10 @@
+// Create a student object with properties: name, age, and marks.
+
+let selfInfo =
+ {
+    name : "Priya",
+    age :10,
+    marks : 20,
+    Course : "Bcs"
+};
+console.log(selfInfo);
