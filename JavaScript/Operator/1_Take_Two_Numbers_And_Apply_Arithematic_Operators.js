65cba33c033ce2a03b825a399dea738253791141
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-11-20 22:45:01 +0530

    Add files via upload

---
+++ JavaScript/Operator/1_Take_Two_Numbers_And_Apply_Arithematic_Operators.js
@@ -0,0 +1,26 @@
+let a = 30;
+let b = 20;
+ let Sum = a+b;
+ console.log(Sum);
+
+ let p = 20;
+ let q = 3;
+ let Sub = p - q;
+ console.log(Sub);
+
+ let A = 10;
+ let B = 5;
+ let Mul = A * B;
+ console.log(Mul);
+
+ let x = 20;
+ let y = 6;
+ let Div = x / y;
+ console.log(Div);
+
+
+
+ let m = 20;
+ let n = 3;
+ let Mod = m % n;
+ console.log (Mod);
