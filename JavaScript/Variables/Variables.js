f2a445334cc17c9f74c5aa4126193dde4acce365
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-11-27 23:36:28 +0530

    Add files via upload

---
+++ JavaScript/Variables/Variables.js
@@ -0,0 +1,15 @@
+
+                    const a= 30;
+                    const b = 10;
+
+                    const c = 30 + 10;
+                    console.log(c);
+
+                    const arr  = [10,20,30,40];
+                    arr[0] = 100;
+                    console.log(arr);
+
+                
+                    const obj = {name:"myname",age:10,std:"1st"};
+                    console.log(obj);
+   
