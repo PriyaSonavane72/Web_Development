899d7497b5c5a73e0e5cd53c5f21224d259191c0
Priya Sonavane <134388979+PriyaSonavane72@users.noreply.github.com>
2025-12-04 23:05:34 +0530

    Add files via upload

---
+++ JavaScript/Statement/While.js
@@ -0,0 +1,18 @@
+
+
+     var Name = "Web Development";
+     var x= 6;
+     var y = 20;
+     do{
+          console.log(Name);
+          x++;
+     }while(x<y)
+
+
+     var Student = "name";   ///////Interview Quetion
+     var x  = 6;
+     var y = 20;
+     do{
+        console.log(Student);
+        x++;
+     }while(x>y)
